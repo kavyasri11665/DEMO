0b8df1e3f70c7da4bb56649939568a4980b58864
kavyasri11665 <132869087+kavyasri11665@users.noreply.github.com>
2024-01-08 12:07:48 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ st.header("types of cats")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("cat1 Cat")
-  st.image("./cat1.jpeg",caption=" Cat1",width=300,use_column_width=true)
+  st.image("./cat1.jpg",caption=" Cat1",width=300,use_column_width=true)
   st.write(" cat1 are cute")
 with col2:
   st.subheader("cat2 Cat")
