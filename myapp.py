8b54ce7bcbe4cc15c0c16c8bd3dbf721c224e7f0
kavyasri11665 <132869087+kavyasri11665@users.noreply.github.com>
2024-01-08 11:59:18 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,13 +3,13 @@ st.set_page_config(page_title='cats')
 st.header("types of cats")
 col1,col2=st.columns(2)
 with col:
-  st.subheader("Persian Cat")
-  st.image("./Persian.jpg",caption="Persian Cat",width=300,use_column_width=true)
-  st.write("Persian cats are cute")
+  st.subheader("cat1 Cat")
+  st.image("./cat1.jpg",caption=" Cat1",width=300,use_column_width=true)
+  st.write(" cat1 are cute")
 with col2:
-  st.subheader("Ragdoll Cat")
-  st.image("./ragdoll.jpg",caption="Ragdoll Cat",width=300,use_column_width=true)
-  st.write("Ragdoll cats are proud")
+  st.subheader("cat2 Cat")
+  st.image("./cat2.jpg",caption=" Cat2",width=300,use_column_width=true)
+  st.write(" cat2 are proud")
                      
 
                    
