ebb32611579bb452690face2df6ca544d72a4f9d
kavyasri11665 <132869087+kavyasri11665@users.noreply.github.com>
2024-01-08 12:15:23 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,13 +3,13 @@ st.set_page_config(page_title='Cats')
 st.header("Types of dogs")
 col1,col2=st.columns(2)
 with col1:
-  st.subheader("French Bulldogs")
+  st.subheader("paresian")
   st.image("./cat1.jpeg",caption="French Bulldogs",width=300,use_column_width=True)
   st.write("French Bulldogs are cute")
 with col2:
-  st.subheader("Golden Retriver")
+  st.subheader("ragdoll")
   st.image("./cat2.jpg",caption="Golden Retriver",width=300,use_column_width=True)
-  st.write("Golden Retriver dogs are cute")
+  st.write("Golden Retriver cat are cute")
                      
 
                    
